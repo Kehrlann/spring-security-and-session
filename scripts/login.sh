9250be73a4bf0ff3b25533894e924043dea4fba2
Daniel Garnier-Moiroux <git@garnier.wf>
2018-07-01 01:40:33 +0200

    Improve login script : remove previous token

--- scripts/login.sh
+++ scripts/login.sh
@@ -3,12 +3,17 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TOKEN_FILE="$SCRIPT_DIR"/token-header.txt
+
+if [ -f "$TOKEN_FILE" ]; then
+  echo 'Removing previous token...'
+  rm "$TOKEN_FILE"
+fi
 
 echo 'Authenticating ...'
 RESPONSE=$(curl http://localhost:8080/login -d username=user -d password=password -i -v 2> /dev/null)
 echo 'Success !'
 
-echo 'Writing token header ...'
 TOKEN=$(grep "X-Auth-Token" <(echo "$RESPONSE"))
-echo $TOKEN > "$SCRIPT_DIR"/token-header.txt
-echo "Wrote token in $SCRIPT_DIR/token-header.txt"
+echo "$TOKEN" > "$TOKEN_FILE"
+echo "Wrote token in $TOKEN_FILE"
