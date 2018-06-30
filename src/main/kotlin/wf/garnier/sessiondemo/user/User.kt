9b2b7e1374c29236374118b3d2a1b8bcae1a8ebb
Daniel Garnier-Moiroux <git@garnier.wf>
2018-07-01 01:49:07 +0200

    do not include encrypted password in user details

--- src/main/kotlin/wf/garnier/sessiondemo/user/User.kt
+++ src/main/kotlin/wf/garnier/sessiondemo/user/User.kt
@@ -4,11 +4,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import com.fasterxml.jackson.annotation.JsonIgnore
 
 @Entity
 class User(@Id @GeneratedValue val id: Long = 0L,
            val username: String = "",
-           val encryptedPassword: String = "") {
+           @JsonIgnore val encryptedPassword: String = "") {
 
     constructor(username: String, clearTextPassword: String):
             this(username = username, encryptedPassword = BCryptPasswordEncoder(11).encode(clearTextPassword))
